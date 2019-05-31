a75ff6a5fb6bce402ca5eeec1432a1e5b01d0f50
Alex Merecka <merecka@gmail.com>
2019-05-31 17:27:29 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -8,10 +8,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string :location
       t.string :theme
       t.float :price
-      t.integer :num_of_employees
-      t.boolean :still_in_business
-      t.datetime :opening_time
-      t.datetime :closing_time
+      t.boolean :family_friendly 
     end
 
   end
